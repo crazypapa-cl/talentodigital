b161d22779014d925ca3ee42891ff7cf54a196fc
Mac-Vicky <vcburgos@gmail.com>
2021-02-08 03:14:45 -0300

    listo

--- M2/Portafolio/js/tirada.js
+++ M2/Portafolio/js/tirada.js
@@ -2,12 +2,15 @@ var btn = document.getElementById("tirar")
 console.log(btn)
 var body = document.body
 var uno = document.getElementById('01')
+var vivo = false
 console.log(btn)
+
 function hola()
 {
     alert('hola')
 }
 btn.addEventListener('click', createNode, true)
+
 /*or (const property in body) {
     console.log(`${property}: ${body[property]}`);
   }
@@ -24,21 +27,42 @@ function tirar(cartas){
     console.log(numero)
     const numeroUnico = [... new Set(numero)]
     console.log(numeroUnico)
-     const tirada = numeroUnico.slice(0,cartas);
+    const tirada = numeroUnico.slice(0,cartas);
     return tirada
 }
 
 function createNode(){
+    if (vivo){
+        clearNode()
+        
+    }else{
+        vivo=true
+    }
     const tiradas = tirar(3);
     console.log(tiradas.length)
     function animatrix(i){
         const newImg = document.createElement('img');
         newImg.setAttribute('class','animar')
         uno.appendChild(newImg).setAttribute('src',`img/${tiradas[i]}.jpg`)
-        }
-    for(var i = 0; i<tiradas.length; i++){
-        
-        setTimeout(animatrix(i), 1000);
+        } var i = 0 
+   /* for(var i = 0; i<tiradas.length; i++){
+            
+        animatrix(i)*/
+        var tiempo = setInterval(function(){
+       
+            if( i < 3){
+                
+                animatrix(i)
+                console.log(i)
+                i++
+
+            }else{
+                i=0
+                console.log('pasando por else')
+                clearInterval(tiempo)
+            }
+       
+        },200)
 }
     
     /*tiradas.forEach(element=>{
@@ -48,11 +72,11 @@ function createNode(){
    setTimeout(newImg.setAttribute('class','animar2'), 5000);
 
     })*/
-} 
-createNode()
 function clearNode(){
     uno.children[0].remove()
     uno.children[1].remove()
     uno.children[0].remove()
 
 }
+console.log(body)
+var nav = '<nav class="flex-sm-column flex-sm-row flex-md-row nav-sm nav-md "><img src="hand_white.svg" class="logo"><div class="flex-md-row flex-column-md  titulo-box-md"><h1 class="titulo-sm ">Tarot</h1><h3 class="subtitulo-sm subtitulo-md">Online</h3></div><ul class="flex-sm-column nav-md nav-mdx-row typo-meta "><li><a href="tirada.html">Tirada</a></li><li><a>Arcano</a></li><li><a href="galeria.html">Galeria</a></li><li><a href="index.html">HOME</a></li></ul></nav>'
