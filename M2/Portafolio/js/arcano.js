d955a9a8d9e48e2392b76913ef2306ea40ac4799
Mac-Vicky <vcburgos@gmail.com>
2021-02-10 10:33:39 -0300

    Pa aariba

--- M2/Portafolio/js/arcano.js
+++ M2/Portafolio/js/arcano.js
@@ -1,12 +1,24 @@
 var valor = document.getElementById('valor')
 var fecha = document.getElementById('fecha')
-console.log(fecha.value)
-
+var lista = new Array()
+var loop = true 
 
 fecha.onchange=function(){
+  lista = []
+  var val = fecha.value
   valor.innerText=fecha.value
   console.log(fecha.value.length)
   console.log(typeof fecha.value)
   console.log(fecha.value.split('-'))
+  loop = false
+  for(i=0; i<val.length; i++){
+    lista.push(val[i])
+    valor.innerText=fecha.value.anchor()
+    }
+    console.log('-----------')
+    console.log(lista)
+ 
+
+
 }
 
