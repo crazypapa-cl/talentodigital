e3c223dc46224ccb8e8b7e728ce024c0eef7e6bb
Mac-Vicky <vcburgos@gmail.com>
2021-02-11 17:52:40 -0300

    tu arcano hand Made

--- M2/Portafolio/js/arcano.js
+++ M2/Portafolio/js/arcano.js
@@ -1,24 +1,42 @@
 var valor = document.getElementById('valor')
 var fecha = document.getElementById('fecha')
+var img = document.getElementById('img')
 var lista = new Array()
-var loop = true 
-
+var na = []
+var carta =0
+numero=0
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
 fecha.onchange=function(){
-  lista = []
-  var val = fecha.value
-  valor.innerText=fecha.value
-  console.log(fecha.value.length)
-  console.log(typeof fecha.value)
-  console.log(fecha.value.split('-'))
-  loop = false
-  for(i=0; i<val.length; i++){
-    lista.push(val[i])
-    valor.innerText=fecha.value.anchor()
-    }
-    console.log('-----------')
-    console.log(lista)
- 
+  text = fecha.value
+  console.log(text)
+  console.log(typeof text)
+  var g1=text.indexOf('-')
+  console.log(g1)
+  console.log(text)
+  console.log(typeof text)
+  console.log(text.slice(0,4))
+  na.push(text.slice(0,4))
+  na.push(text.slice(5,7))
+  na.push(text.slice(8,10))
+  text=na[0]+na[1]+na[2]
+  console.log(text)
+  for (i=0; i < text.length;i++) {
+    carta += parseInt(text[i],10)
+  }
+  console.log(carta)
+  if(carta>22){
+    var text = carta.toString()
+    console.log('es mayor a 22')
+    numero += parseInt(text[0]) + parseInt(text[1]);
+    console.log(numero)
 
+  }
+  img.setAttribute('src',`img/${numero}.jpg`);
+  img.setAttribute('class','arcano')
 
 }
 
+
+/*na.forEach(function(elemento, indice, array) {
+  console.log(elemento, indice);
+})*/
