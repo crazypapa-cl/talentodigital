5324b94a8a029fed1d0bd01a775feef5a0c3b661
Alex Carrasco <alex.carrascob@gmail.com>
2021-05-30 21:10:32 -0400

    Completar página de integrantes del curso. Cambiar levemente la distribución de elementos y el diseño de la página principal.

---
+++ integrantes/js/script.js
@@ -0,0 +1,89 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        tutores: [
+            {
+                nombre: 'Rodrigo Gálvez',
+                userGitHub: 'rodrigogalvez',
+                linkGitHub: 'https://github.com/rodrigogalvez'
+            },
+            {
+                nombre: 'Pablo Aravena',
+                userGitHub: 'pabloaravenaleon',
+                linkGitHub: 'https://github.com/pabloaravenaleon'
+            }
+        ],
+        alumnos: [
+            {
+                nombre: 'Aldo Yáñez',
+                userGitHub: 'AldousTheWise',
+                linkGitHub: 'https://github.com/AldousTheWise'
+            },
+            {
+                nombre: 'Alex Carrasco',
+                userGitHub: 'alexcarrbarr',
+                linkGitHub: 'https://github.com/alexcarrbarr'
+            },
+            {
+                nombre: 'Andrea Oyarce',
+                userGitHub: 'Andrea-Oyarce-Espinoza',
+                linkGitHub: 'https://github.com/Andrea-Oyarce-Espinoza'
+            },
+            {
+                nombre: 'Cristina Nuñez',
+                userGitHub: 'crizabell',
+                linkGitHub: 'https://github.com/crizabell'
+            },
+            {
+                nombre: 'Hithamar Durán',
+                userGitHub: '',
+                linkGitHub: ''
+            },
+            {
+                nombre: 'Javiera Kammle',
+                userGitHub: '',
+                linkGitHub: ''
+            },
+            {
+                nombre: 'Jesús Pardo',
+                userGitHub: '',
+                linkGitHub: ''
+            },
+            {
+                nombre: 'Joaquín Jaramillo',
+                userGitHub: '',
+                linkGitHub: ''
+            },
+            {
+                nombre: 'Jorge Galleguillos',
+                userGitHub: '',
+                linkGitHub: ''
+            },
+            {
+                nombre: 'Leandro Venegas',
+                userGitHub: 'crazypapa-cl',
+                linkGitHub: 'https://github.com/crazypapa-cl'
+            },
+            {
+                nombre: 'Mauricio Fernández',
+                userGitHub: '',
+                linkGitHub: ''
+            },
+            {
+                nombre: 'Nicolás Zapata',
+                userGitHub: 'nicolas-aleph',
+                linkGitHub: 'https://github.com/nicolas-aleph'
+            },
+            {
+                nombre: 'Paola Gil',
+                userGitHub: '',
+                linkGitHub: ''
+            },
+            {
+                nombre: 'Rodrigo Carrasco',
+                userGitHub: 'rodrigobulboa',
+                linkGitHub: 'https://github.com/rodrigobulboa'
+            }
+        ]
+    }
+})
